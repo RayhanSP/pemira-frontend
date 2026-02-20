bd0dc310fd308393798e16ee391ce1cfec1e43a4
RayhanSP <ray.han2005.rh@gmail.com>
2026-02-20 17:36:59 +0700

    fix: allow github image domains in next config

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,24 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: false,
   images: {
-    remotePatterns: [{ hostname: 'djaeger.s3.ap-southeast-1.amazonaws.com' }],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'djaeger.s3.ap-southeast-1.amazonaws.com', // Bawaan asli kamu
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co', // Untuk placeholder (jika masih ada)
+      },
+      {
+        protocol: 'https',
+        hostname: 'github.com', // Untuk link direct awal dari GitHub Issues
+      },
+      {
+        protocol: 'https',
+        hostname: '**.githubusercontent.com', // Untuk menangkap file redirect/CDN dari GitHub
+      }
+    ],
   },
 }
 
